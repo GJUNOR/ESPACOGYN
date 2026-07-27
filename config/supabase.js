5deb22364d8e3f5e526aaad18568ccb4d1ef37b5
Gustavo Vieira <gustavoacupuntura@hotmail.com>
2026-07-27 16:54:17 -0300

    corrigir url supabase

--- config/supabase.js
+++ config/supabase.js
@@ -1,15 +1,12 @@
 require("dotenv").config();
 
-console.log("========== SUPABASE DEBUG ==========");
-console.log("URL:", process.env.SUPABASE_URL);
-console.log("KEY EXISTE:", !!process.env.SUPABASE_KEY);
-console.log("====================================");
-
 const { createClient } = require("@supabase/supabase-js");
+
 
 const supabase = createClient(
     process.env.SUPABASE_URL,
     process.env.SUPABASE_KEY
 );
+
 
 module.exports = supabase;
