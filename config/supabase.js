5ae984b73640b3fcd901145c299e5cc1722cdb29
Gustavo Vieira <gustavoacupuntura@hotmail.com>
2026-07-27 16:39:16 -0300

    debug url supabase

--- config/supabase.js
+++ config/supabase.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 
-console.log("ENV SUPABASE_URL:", process.env.SUPABASE_URL);
-console.log("ENV SUPABASE_KEY EXISTE:", !!process.env.SUPABASE_KEY);
+console.log("========== SUPABASE DEBUG ==========");
+console.log("URL:", process.env.SUPABASE_URL);
+console.log("KEY EXISTE:", !!process.env.SUPABASE_KEY);
+console.log("====================================");
 
 const { createClient } = require("@supabase/supabase-js");
 
