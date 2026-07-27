c21d4de59795842bfaa9f9370b496efe100716f6
Gustavo Vieira <gustavoacupuntura@hotmail.com>
2026-07-27 16:33:19 -0300

    teste url supabase

--- config/supabase.js
+++ config/supabase.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 
+console.log("URL SUPABASE LIDA:", process.env.SUPABASE_URL);
+
 const { createClient } = require("@supabase/supabase-js");
 
 const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_KEY
 );
 
 module.exports = supabase;
